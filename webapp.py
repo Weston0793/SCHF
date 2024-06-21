0497d0c8d1ea45e764985f7b1bb9573db5146672
Weston0793 <114341000+Weston0793@users.noreply.github.com>
2024-06-22 00:23:07 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -60,18 +60,13 @@ if uploaded_file is not None:
             transforms.ToTensor()
         ])
         image_tensor = transform(enhanced_image).unsqueeze(0).to(device)
-        cropped_tensor, _ = autocrop_image(image_tensor, crop_model, device)
+        cropped_image_pil, _ = autocrop_image(image_tensor, crop_model, device)
         
-        if cropped_tensor is not None:
-            # Convert tensor to PIL image for display
-            cropped_image_pil = transforms.ToPILImage()(cropped_tensor.squeeze(0))
+        if cropped_image_pil is not None:
             st.image(cropped_image_pil, caption='Cropped X-Ray', use_column_width=True)
         else:
             st.warning("No region of interest found. Using original image.")
-            cropped_tensor = image_tensor
-
-        # Convert tensor to PIL image for dataset creation
-        cropped_image_pil = transforms.ToPILImage()(cropped_tensor.squeeze(0))
+            cropped_image_pil = enhanced_image
 
         # Create datasets with and without augmentation
         dataloader_lion = DataLoader(create_transformed_dataset(cropped_image_pil, batch_size=20, augment=True), batch_size=1)
@@ -85,8 +80,8 @@ if uploaded_file is not None:
         st.markdown(f"<div style='border:2px solid #000; padding: 10px; background-color: #f0f0f0;'><strong>Prediction:</strong> {prediction}<br><strong>Confidence:</strong> {confidence:.2f}</div>", unsafe_allow_html=True)
 
         # Generate and display CAM on the cropped image
-        if cropped_tensor is not None:
-            cropped_image_rgb = transforms.ToPILImage()(cropped_tensor.squeeze(0)).convert('RGB')
+        if cropped_image_pil is not None:
+            cropped_image_rgb = cropped_image_pil.convert('RGB')  # Ensure the image is in RGB format
             img_tensor = transforms.ToTensor()(cropped_image_rgb).unsqueeze(0).to(device)
             cam = get_cam(lion_model if prediction == "Fractured Pediatric Supracondylar Humerus" else swdsgd_model, img_tensor, 'base_model.features')
             cam_image = apply_cam_on_image(np.array(cropped_image_rgb), cam)
