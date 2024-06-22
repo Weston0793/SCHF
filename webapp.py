6e8f074a3b2477b87d45c24b09123a92e65e251a
Weston0793 <114341000+Weston0793@users.noreply.github.com>
2024-06-22 02:37:44 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,6 +10,7 @@ from skimage import exposure
 from cam_helper import get_cam, apply_cam_on_image
 from predict import create_transformed_dataset, load_models, predict_fracture, CustomDataset
 from torch.utils.data import DataLoader
+from enhancements import adaptive_histogram_equalization, sharpen_image, contrast_stretching
 
 # Initialize models
 lion_model = BinaryMobileNetV3Small()
@@ -20,24 +21,6 @@ crop_model = ResNetUNet()
 models_folder = "models"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 lion_model, swdsgd_model, crop_model = load_models(lion_model, swdsgd_model, crop_model, models_folder, device)
-
-# Define new image enhancement functions
-def adaptive_histogram_equalization(image, clip_limit=2.0, tile_grid_size=(8, 8)):
-    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
-    equalized_image = clahe.apply(np.array(image))
-    return Image.fromarray(equalized_image)
-
-def sharpen_image(image, sigma=1, alpha=1.5, beta=-0.5):
-    image_np = np.array(image)
-    blurred = cv2.GaussianBlur(image_np, (0, 0), sigma)
-    sharpened = cv2.addWeighted(image_np, alpha, blurred, beta, 0)
-    return Image.fromarray(sharpened)
-
-def contrast_stretching(image):
-    image_np = np.array(image)
-    p2, p98 = np.percentile(image_np, (2, 98))
-    img_rescale = exposure.rescale_intensity(image_np, in_range=(p2, p98))
-    return Image.fromarray(img_rescale)
 
 # Streamlit app layout
 st.title("Pediatric Supracondylar Humerus Fracture X-Ray Classification with Twin Network")
