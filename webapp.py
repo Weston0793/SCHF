fa8cf9cb3d13d63d4ab70d8576c17a77f0d4124b
Weston0793 <114341000+Weston0793@users.noreply.github.com>
2024-06-22 03:09:24 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -24,6 +24,22 @@ lion_model, swdsgd_model, crop_model = load_models(lion_model, swdsgd_model, cro
 
 # Streamlit app layout
 st.title("Pediatric Supracondylar Humerus Fracture X-Ray Classification with Twin Network")
+
+# Style for larger text and highlighted prediction box
+st.markdown("""
+    <style>
+    .large-text {
+        font-size: 1.25rem;
+    }
+    .prediction-box {
+        border: 2px solid #000;
+        padding: 20px;
+        background-color: #f0f0f0;
+        font-size: 1.5rem;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 uploaded_file = st.file_uploader("Upload X-Ray Image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
@@ -62,7 +78,7 @@ if uploaded_file is not None:
         prediction, confidence = predict_fracture(lion_model, swdsgd_model, dataloader_lion, dataloader_swdsgd, device)
 
         # Display the prediction in a highlighted box
-        st.markdown(f"<div style='border:2px solid #000; padding: 10px; background-color: #f0f0f0;'><strong>Prediction:</strong> {prediction}<br><strong>Confidence:</strong> {confidence:.2f}</div>", unsafe_allow_html=True)
+        st.markdown(f"<div class='prediction-box'><strong>Prediction:</strong> {prediction}<br><strong>Confidence:</strong> {confidence:.2f}</div>", unsafe_allow_html=True)
 
         # Generate and display CAM on the cropped image
         if cropped_image_pil is not None:
@@ -76,3 +92,17 @@ if uploaded_file is not None:
             st.image(cam_image, caption='Class Activation Map (CAM) on Cropped Image', use_column_width=True)
     except Exception as e:
         st.error(f"An error occurred: {e}")
+
+# Additional Information
+st.markdown("""
+    ### Authors:
+    Aba Lőrincz 1,2,3, András Kedves2, Hermann Nudelman1,3, András Garami1, Gergő Józsa 1,3,* and Zsolt Kisander2
+    
+    ### Affiliations:
+    1. Department of Thermophysiology, Institute for Translational Medicine, Medical School, University of Pécs, 12 Szigeti Street, H7624 Pécs, Hungary; aba.lorincz@gmail.com (AL); andras.garami@aok.pte.hu (AG)
+    2. Department of Automation, Faculty of Engineering and Information Technology, University of Pécs, 2 Boszorkány Street, H Pécs, Hungary; (AK) (Zs.K)
+    3. Division of Surgery, Traumatology, Urology, and Otorhinolaryngology, Department of Paediatrics, Clinical Complex, University of Pécs, 7 József Attila Street, H7623 Pécs, Hungary; jozsa.gergo@pte.hu (GJ)
+
+    ### Code:
+    [GitHub Repository](https://github.com/Weston0793/SCHF/)
+""", unsafe_allow_html=True)
